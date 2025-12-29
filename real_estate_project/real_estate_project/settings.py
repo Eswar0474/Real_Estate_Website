9fa4dbfb25a25e6e15addade90fa0fd60c196844
Eswar <ceepurameswar0474@gmail.com>
2025-12-29 15:20:25 +0530

    Update settings.py

--- real_estate_project/real_estate_project/settings.py
+++ real_estate_project/real_estate_project/settings.py
@@ -96,6 +96,9 @@ TEMPLATES = [
 # --------------------------------------------------
 # DATABASE (Render PostgreSQL + Local SQLite fallback)
 # --------------------------------------------------
+# --------------------------------------------------
+# DATABASE
+# --------------------------------------------------
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
@@ -104,16 +107,24 @@ DATABASES = {
 }
 
 
-
 # --------------------------------------------------
 # PASSWORD VALIDATION
 # --------------------------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
+
 
 
 # --------------------------------------------------
